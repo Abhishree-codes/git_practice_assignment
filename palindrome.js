de2c38b75edf62616bc931c0df26055694fe378c
Abhishree Tripathi <abhishreetripathi@192.168.1.4>
2023-02-11 00:07:40 +0530

    I have written prime number code and palindrome code

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+let str2="";
+for(let i=str.length-1; i>=0; i++){
+  str2>str2+str[i];
+}
+if(str2>str){
+  console.log("palindrome");
+}
+else{
+  console.log("not palindrome");
+}
---
+++ prime.js
@@ -0,0 +1,10 @@
+let count=0;
+for(let i=4; i<=num; I++){
+if (num%i==0){
+Count +=8;
+}
+}
+if (count==2){
+console.log("prime");
+}
+else{ console.log("not prime");
